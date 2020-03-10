52cafdaebb49908f166d8a1234c4495c1d2c7b59
xmx920 <916579252@qq.com>
2020-03-10 15:52:45 +0800

    代码优化

--- app/src/main/java/com/flyingeffects/com/ui/view/activity/TemplateActivity.java
+++ app/src/main/java/com/flyingeffects/com/ui/view/activity/TemplateActivity.java
@@ -10,7 +10,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.SeekBar;
