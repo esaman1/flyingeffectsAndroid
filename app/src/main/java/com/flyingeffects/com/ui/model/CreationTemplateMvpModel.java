b2e093c979f1e8c18959ae12777aa7bb470f59e3
xmx920 <916579252@qq.com>
2020-03-19 17:57:23 +0800

    代码优化

--- app/src/main/java/com/flyingeffects/com/ui/model/CreationTemplateMvpModel.java
+++ app/src/main/java/com/flyingeffects/com/ui/model/CreationTemplateMvpModel.java
@@ -148,7 +148,7 @@ public class CreationTemplateMvpModel {
         stickView.setRightTopBitmap(context.getDrawable(R.mipmap.sticker_copy));
         stickView.setLeftTopBitmap(context.getDrawable(R.drawable.sticker_delete));
         stickView.setRightBottomBitmap(context.getDrawable(R.mipmap.sticker_redact));
-        stickView.setImageRes(gifdownSticker, false);
+        stickView.setImageRes(path, false);
         AnimStickerModel animStickerModel = new AnimStickerModel(context, viewLayerRelativeLayout, stickView);
         listForStickerView.add(animStickerModel);
         callback.ItemClickForStickView(animStickerModel);
