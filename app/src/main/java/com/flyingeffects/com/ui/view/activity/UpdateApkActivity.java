3a7276733c8b2084400c61b24242a0aa23da49f0
xmx920 <916579252@qq.com>
2020-04-08 12:58:09 +0800

    检查更新代码优化

--- app/src/main/java/com/flyingeffects/com/ui/view/activity/UpdateApkActivity.java
+++ app/src/main/java/com/flyingeffects/com/ui/view/activity/UpdateApkActivity.java
@@ -130,12 +130,12 @@ public class UpdateApkActivity extends Activity implements OnClickListener {
             // conn.getContentLength();
             InputStream is = conn.getInputStream();
             //新建一个File，传入文件夹目录
-            File file_instalApk = new File(Environment.getExternalStorageDirectory() + "/kadian");
+            File file_instalApk = new File(Environment.getExternalStorageDirectory() + "/feishan");
             //判断文件夹是否存在，如果不存在就创建，否则不创建
             if (!file_instalApk.exists()) {
                 file_instalApk.mkdirs();
             }
-            File file = new File(Environment.getExternalStorageDirectory() + "/kadian", "kadian" + ".apk");
+            File file = new File(Environment.getExternalStorageDirectory() + "/feishan", "feishan" + ".apk");
             fos = new FileOutputStream(file);
             BufferedInputStream bis = new BufferedInputStream(is);
             byte[] buffer = new byte[1024];
@@ -255,7 +255,7 @@ public class UpdateApkActivity extends Activity implements OnClickListener {
             // 由于没有在Activity环境下启动Activity,设置下面的标签
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
-            Uri apkUri = FileProvider.getUriForFile(this, "com.mobile.kadian.fileprovider", file);
+            Uri apkUri = FileProvider.getUriForFile(this, "com.flyingeffects.com.fileprovider", file);
             //添加这一句表示对目标应用临时授权该Uri所代表的文件
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
