03a49df16d37c8ea883bc50853484cfa0143e254
ztj740740 <wasd1234>
2020-06-24 15:40:28 +0800

    代码优化

---
+++ app/src/main/java/com/flyingeffects/com/ui/view/activity/PreviewUpAndDown.java
@@ -0,0 +1,36 @@
+package com.flyingeffects.com.ui.view.activity;
+
+import com.flyingeffects.com.R;
+import com.flyingeffects.com.base.BaseActivity;
+import com.lmx.library.media.VideoPlayRecyclerView;
+
+
+/**
+ * description ：仿抖音效果的预览页面
+ * creation date: 2020/6/24
+ * user : zhangtongju
+ */
+public class PreviewUpAndDown extends BaseActivity {
+
+    private VideoPlayRecyclerView mRvVideo;
+
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.act_preview_up_and_down;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initAction() {
+
+    }
+
+
+
+}
