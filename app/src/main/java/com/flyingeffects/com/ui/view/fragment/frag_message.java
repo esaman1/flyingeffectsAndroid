50bed4bdb64f65f4cafe71dd923c13b63f60ff96
ztj740740 <wasd1234>
2020-07-28 14:08:17 +0800

    2.9.0代码开始开发

---
+++ app/src/main/java/com/flyingeffects/com/ui/view/fragment/frag_message.java
@@ -0,0 +1,34 @@
+package com.flyingeffects.com.ui.view.fragment;
+
+import com.flyingeffects.com.R;
+import com.flyingeffects.com.base.BaseFragment;
+
+
+
+/**
+ * description ：消息页面
+ * creation date: 2020/7/28
+ * user : zhangtongju
+ */
+public class frag_message extends BaseFragment {
+
+    @Override
+    protected int getContentLayout() {
+        return R.layout.fag_message;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initAction() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
