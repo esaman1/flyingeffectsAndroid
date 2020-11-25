2672a4017cffc65d056fe8674cabd786d6c559e5
ztj740740 <wasd1234>
2020-11-25 09:50:15 +0800

    代码优化

--- app/src/main/java/com/flyingeffects/com/ui/view/activity/CreationTemplatePreviewActivity.java
+++ app/src/main/java/com/flyingeffects/com/ui/view/activity/CreationTemplatePreviewActivity.java
@@ -282,9 +282,9 @@ public class CreationTemplatePreviewActivity extends BaseActivity implements Cre
                 if (isPlaying()) {
                     videoPause();
                 }
-//                else {
-//                    videoResume();
-//                }
+                else {
+                    videoResume();
+                }
                 if (!isShowPreviewAd && BaseConstans.getHasAdvertising() == 1 && !BaseConstans.getIsNewUser()) {
                     AdManager.getInstance().showCpAd(this, AdConfigs.AD_SCREEN_FOR_PREVIEW);
                     isShowPreviewAd = true;
