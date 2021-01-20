ff346414ed166fb3af5e05537ea3e76a696672d2
ztj740740 <wasd1234>
2021-01-20 09:40:45 +0800

    4.0正式版

--- app/src/main/java/com/flyingeffects/com/base/BaseApplication.java
+++ app/src/main/java/com/flyingeffects/com/base/BaseApplication.java
@@ -98,7 +98,7 @@ public class BaseApplication extends MultiDexApplication {
                         .KSAppKey("517200002")
                         .KaiJiaAppKey("68662a49")
                         .GDTAppKey(AdConfigs.APP_ID_GDT)
-                        .isDebug(true)
+                        .isDebug(false)
                         .build());
     }
 
@@ -139,7 +139,7 @@ public class BaseApplication extends MultiDexApplication {
      * 中台
      */
     private void initZt() {
-        NTAnalytics.setDebug(true);
+        NTAnalytics.setDebug(false);
         NTAnalytics.init(this, "87", "vQlTNPzHOzBYHzkg", ChannelUtil.getChannel(this));
     }
 
