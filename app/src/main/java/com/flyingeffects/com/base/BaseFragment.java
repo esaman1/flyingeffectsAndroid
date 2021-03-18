8c86935dd5d716466848525d9031612640db3991
ztj740740 <wasd1234>
2021-03-18 14:29:46 +0800

    bug 修改

--- app/src/main/java/com/flyingeffects/com/base/BaseFragment.java
+++ app/src/main/java/com/flyingeffects/com/base/BaseFragment.java
@@ -342,12 +342,12 @@ public abstract class BaseFragment extends Fragment implements IActivity {
     /**
      * 当前主页选择的位数
      */
-    public int NowHomePageChooseNum = 1;
+    public static int NowHomePageChooseNum = 1;
 
     /**
      * 当前第二页选择位数
      */
-    public int NowSecondChooseNum = 0;
+    public static int NowSecondChooseNum = 0;
 
 
     public boolean HasShowAd = false;
@@ -357,6 +357,7 @@ public abstract class BaseFragment extends Fragment implements IActivity {
     public void onEventMainThread(HomeChoosePageListener listener) {
         if (getActivity() != null) {
             NowHomePageChooseNum = listener.getPager();
+            LogUtil.d("pageChange","NowHomePageChooseNum="+NowHomePageChooseNum);
             if(callback!=null){
                 callback.isChange();
             }
@@ -369,6 +370,7 @@ public abstract class BaseFragment extends Fragment implements IActivity {
     public void onEventMainThread(SecondChoosePageListener listener) {
         if (getActivity() != null) {
             NowSecondChooseNum = listener.getPager();
+            LogUtil.d("pageChange","NowSecondChooseNum="+NowSecondChooseNum);
             if(callback!=null){
                 callback.isChange();
             }
