db69b817948656c40f78e97235b2dc8a49028810
ztj740740 <wasd1234>
2021-04-08 09:51:27 +0800

    test

---
+++ app/src/main/java/com/flyingeffects/com/ui/view/activity/test.kt
@@ -0,0 +1,33 @@
+package com.flyingeffects.com.ui.view.activity
+
+import com.flyingeffects.com.R
+import com.flyingeffects.com.base.BaseActivity
+
+class test : BaseActivity() {
+
+
+
+
+
+    override fun initAction() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLayoutId(): Int {
+      return  R.layout.act_test
+    }
+
+
+    /**
+     * 添加view
+     */
+    fun addView() {
+
+    }
+
+
+}
