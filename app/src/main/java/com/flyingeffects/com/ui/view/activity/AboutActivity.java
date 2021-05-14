2e9bb8cbfc16b9a326b0f186b7f2ec7b7dca30e0
shijiaqi <496468012@qq.com>
2021-05-14 16:05:43 +0800

    about页面butterknife改写

--- app/src/main/java/com/flyingeffects/com/ui/view/activity/AboutActivity.java
+++ app/src/main/java/com/flyingeffects/com/ui/view/activity/AboutActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,37 +17,47 @@ import com.flyingeffects.com.R;
 import com.flyingeffects.com.base.BaseActivity;
 import com.flyingeffects.com.base.BaseApplication;
 import com.flyingeffects.com.constans.BaseConstans;
+import com.flyingeffects.com.databinding.ActAboutBinding;
+import com.flyingeffects.com.databinding.ViewTopBinding;
 import com.flyingeffects.com.manager.DataCleanManager;
 import com.flyingeffects.com.manager.StatisticsEventAffair;
 import com.flyingeffects.com.ui.view.dialog.CommonMessageDialog;
+import com.flyingeffects.com.utils.LogUtil;
 import com.flyingeffects.com.utils.PermissionUtil;
 import com.flyingeffects.com.utils.SystemUtil;
 import com.flyingeffects.com.utils.ToastUtil;
 
-import butterknife.BindView;
-import butterknife.OnClick;
 
 /**
  * 关于界面
  */
 public class AboutActivity extends BaseActivity {
 
-    @BindView(R.id.tv_version_number)
-    TextView tvVersionNumber;
 
     private Context mContext;
+    private ActAboutBinding mBinding;
+    private ViewTopBinding mTopBinding;
 
     @Override
     protected int getLayoutId() {
-        return R.layout.act_about;
+        return 0;
     }
 
     @Override
     protected void initView() {
         mContext = AboutActivity.this;
-        ((TextView) findViewById(R.id.tv_top_title)).setText("关于");
-        tvVersionNumber.setText("飞闪版本 " + SystemUtil.getVersionName(this));
+
+        mBinding = ActAboutBinding.inflate(getLayoutInflater());
+        View rootView = mBinding.getRoot();
+        mTopBinding = ViewTopBinding.bind(rootView);
+        setContentView(rootView);
+
+        mTopBinding.tvTopTitle.setText("关于");
+        mBinding.tvVersionNumber.setText("飞闪版本 " + SystemUtil.getVersionName(this));
+
+        setOnClickListener();
     }
+
 
     @Override
     protected void initAction() {
@@ -57,91 +68,76 @@ public class AboutActivity extends BaseActivity {
     protected void onResume() {
         super.onResume();
         if (BaseConstans.hasLogin()) {
-            findViewById(R.id.tv_top_submit).setVisibility(View.VISIBLE);
-            ((TextView) findViewById(R.id.tv_top_submit)).setText("退出登录");
-            ((TextView) findViewById(R.id.tv_top_submit)).setTextColor(Color.parseColor("#FE2C55"));
-        }else{
-            findViewById(R.id.tv_top_submit).setVisibility(View.GONE);
+            mTopBinding.tvTopSubmit.setVisibility(View.VISIBLE);
+            mTopBinding.tvTopSubmit.setText("退出登录");
+            mTopBinding.tvTopSubmit.setTextColor(Color.parseColor("#FE2C55"));
+        } else {
+            mTopBinding.tvTopSubmit.setVisibility(View.GONE);
         }
     }
 
-    @Override
-    @OnClick({R.id.tv_test,R.id.tv_top_submit, R.id.iv_top_back, R.id.tv_close_account, R.id.tv_contact_us,R.id.tv_relation_us,
-             R.id.tv_privacy_policy, R.id.tv_protocol, R.id.tv_clear_cache,R.id.tv_notification_management,R.id.tv_version_number})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.tv_close_account:
-                if (!BaseConstans.hasLogin()) {
-                    ToastUtil.showToast(getString(R.string.have_not_login));
-                } else {
-                    goActivity(SignoutActivity.class);
-                }
-                break;
-            case R.id.iv_top_back:
-                this.finish();
-                break;
-            case R.id.tv_contact_us:
-                StatisticsEventAffair.getInstance().setFlag(this, "3_Evaluation");
-                reception();
+    private void setOnClickListener() {
+        mBinding.tvTest.setOnClickListener(this::onViewClick);
+        mTopBinding.tvTopSubmit.setOnClickListener(this::onViewClick);
+        mTopBinding.ivTopBack.setOnClickListener(this::onViewClick);
+        mBinding.tvCloseAccount.setOnClickListener(this::onViewClick);
+        mBinding.tvContactUs.setOnClickListener(this::onViewClick);
+        mBinding.tvRelationUs.setOnClickListener(this::onViewClick);
+        mBinding.tvPrivacyPolicy.setOnClickListener(this::onViewClick);
+        mBinding.tvProtocol.setOnClickListener(this::onViewClick);
+        mBinding.tvClearCache.setOnClickListener(this::onViewClick);
+        mBinding.tvNotificationManagement.setOnClickListener(this::onViewClick);
+        mBinding.tvVersionNumber.setOnClickListener(this::onViewClick);
+    }
 
-                break;
-            case R.id.tv_relation_us:
-                StatisticsEventAffair.getInstance().setFlag(this, "3_contact");
-
-                contactUs();
-                break;
-            //隐私政策
-            case R.id.tv_privacy_policy:
-                Intent intentPrivacy = new Intent(this, webViewActivity.class);
-                intentPrivacy.putExtra("webUrl", BaseConstans.PRIVACYPOLICY);
-                startActivity(intentPrivacy);
-
-                break;
-            //用户协议
-            case R.id.tv_protocol:
-                Intent intentProtocol = new Intent(this, webViewActivity.class);
-                intentProtocol.putExtra("webUrl", BaseConstans.PROTOCOL);
-                startActivity(intentProtocol);
-                break;
-            case R.id.tv_clear_cache:
-                //清除外部cache下的内容
-                DataCleanManager.cleanExternalCache();
-                //清理内部cache
-                DataCleanManager.cleanInternalCache(BaseApplication.getInstance());
-                //清理内部sdk
-                DataCleanManager.cleanFiles(BaseApplication.getInstance());
-                DataCleanManager.cleanExternalFile();
-                ToastUtil.showToast("清理成功");
-                break;
-            case R.id.tv_top_submit:
-                //退出
-                showDialog();
-                break;
-
-            case R.id.tv_test:
-                //抠图测试
+    private void onViewClick(View view) {
+        if (view == mBinding.tvCloseAccount) {
+            if (!BaseConstans.hasLogin()) {
+                ToastUtil.showToast(getString(R.string.have_not_login));
+            } else {
+                goActivity(SignoutActivity.class);
+            }
+        } else if (view == mTopBinding.ivTopBack) {
+            finish();
+        } else if (view == mTopBinding.tvTopSubmit) {
+            showDialog();
+        } else if (view == mBinding.tvTest) {
+            //抠图测试
 //                AlbumManager.chooseVideo(this, 1, 1, (tag, paths, isCancel, albumFileList) -> {
 //                    VideoMattingModel videoMattingModel=new VideoMattingModel(paths.get(0),AboutActivity.this);
 //                    videoMattingModel.addLansongCompoundVideo(paths.get(0));
 //                }, "");
-                break;
-            case R.id.tv_notification_management:
-                StatisticsEventAffair.getInstance().setFlag(AboutActivity.this, "3_notifications");
-                PermissionUtil.gotoPermission(AboutActivity.this);
-                break;
-            case R.id.tv_version_number:
-                StatisticsEventAffair.getInstance().setFlag(mContext, "3_update");
-                SystemUtil.openMarket(this);
-                break;
-            default:
-                break;
-
-
+        } else if (view == mBinding.tvContactUs) {
+            StatisticsEventAffair.getInstance().setFlag(this, "3_Evaluation");
+            reception();
+        } else if (view == mBinding.tvRelationUs) {
+            StatisticsEventAffair.getInstance().setFlag(this, "3_contact");
+            contactUs();
+        } else if (view == mBinding.tvPrivacyPolicy) {
+            Intent intentPrivacy = new Intent(this, webViewActivity.class);
+            intentPrivacy.putExtra("webUrl", BaseConstans.PRIVACYPOLICY);
+            startActivity(intentPrivacy);
+        } else if (view == mBinding.tvProtocol) {
+            Intent intentProtocol = new Intent(this, webViewActivity.class);
+            intentProtocol.putExtra("webUrl", BaseConstans.PROTOCOL);
+            startActivity(intentProtocol);
+        } else if (view == mBinding.tvClearCache) {
+            //清除外部cache下的内容
+            DataCleanManager.cleanExternalCache();
+            //清理内部cache
+            DataCleanManager.cleanInternalCache(BaseApplication.getInstance());
+            //清理内部sdk
+            DataCleanManager.cleanFiles(BaseApplication.getInstance());
+            DataCleanManager.cleanExternalFile();
+            ToastUtil.showToast("清理成功");
+        } else if (view == mBinding.tvNotificationManagement) {
+            StatisticsEventAffair.getInstance().setFlag(AboutActivity.this, "3_notifications");
+            PermissionUtil.gotoPermission(AboutActivity.this);
+        } else if (view == mBinding.tvVersionNumber) {
+            StatisticsEventAffair.getInstance().setFlag(mContext, "3_update");
+            SystemUtil.openMarket(this);
         }
-
     }
-
-
 
     private void showDialog() {
         CommonMessageDialog.getBuilder(mContext)
--- app/src/main/java/com/flyingeffects/com/ui/view/dialog/CommonMessageDialog.java
+++ app/src/main/java/com/flyingeffects/com/ui/view/dialog/CommonMessageDialog.java
@@ -26,7 +26,6 @@ public class CommonMessageDialog extends Dialog implements LifecycleObserver {
     public static final int AD_STATUS_BOTTOM = 2;
     public static final int AD_STATUS_NONE = 0;
 
-
     protected CommonMessageDialog(@NonNull Context context, int themeResId) {
         super(context, themeResId);
     }
