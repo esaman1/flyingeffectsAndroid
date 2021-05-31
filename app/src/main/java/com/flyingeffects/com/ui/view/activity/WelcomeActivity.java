1e3c26e824b8fe083dc8d322738d40aac9b717ae
ztj740740 <wasd1234>
2021-05-31 16:08:55 +0800

    开屏权限优化

--- app/src/main/java/com/flyingeffects/com/ui/view/activity/WelcomeActivity.java
+++ app/src/main/java/com/flyingeffects/com/ui/view/activity/WelcomeActivity.java
@@ -212,10 +212,23 @@ public class WelcomeActivity extends BaseActivity {
 
     private void getPermission() {
         mBinding.rlAdContainer.post(() -> {
-            if (Build.VERSION.SDK_INT >= BUILD_VERSION) {
-                checkPermission();
-                StatisticsEventAffair.getInstance().setFlag(WelcomeActivity.this, "test_ad_into_checkPermiss_6");
-            } else {
+            if(canGetPermissions()){
+                BaseConstans.setLastRequestPerTime(System.currentTimeMillis());
+                if (Build.VERSION.SDK_INT >= BUILD_VERSION) {
+                    checkPermission();
+                    StatisticsEventAffair.getInstance().setFlag(WelcomeActivity.this, "test_ad_into_checkPermiss_6");
+                } else {
+                    hasPermission = true;
+                    StatisticsEventAffair.getInstance().setFlag(WelcomeActivity.this, "test_ad_into_checkPermiss_less_6");
+                    if (BaseConstans.getHasAdvertising() == 1 ) {
+                        StatisticsEventAffair.getInstance().setFlag(WelcomeActivity.this, "test_ad_into_checkPermiss_less_6_requestAd");
+                        showSplashAd();
+                    } else {
+                        noQueryAdReason();
+                        StatisticsEventAffair.getInstance().setFlag(WelcomeActivity.this, "test_ad_into_checkPermiss_less_6_no_requestAd");
+                    }
+                }
+            }else{
                 hasPermission = true;
                 StatisticsEventAffair.getInstance().setFlag(WelcomeActivity.this, "test_ad_into_checkPermiss_less_6");
                 if (BaseConstans.getHasAdvertising() == 1 ) {
@@ -494,6 +507,20 @@ public class WelcomeActivity extends BaseActivity {
     }
 
 
+
+
+    /**
+     * description ：是否可以请求权限，7天后才能请求
+     * creation date: 2021/5/21
+     * user : zhangtongju
+     */
+    private boolean canGetPermissions() {
+        long nowCurrentTime = System.currentTimeMillis();
+        long lastCloseTime = BaseConstans.getLastRequestPerTime();
+        long intervalTime = nowCurrentTime - lastCloseTime;
+        intervalTime = intervalTime / 1000 / 60 / 60 / 24;
+        return intervalTime >= 24*7;
+    }
 
 
 
